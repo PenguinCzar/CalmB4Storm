6c15b8a5eb0f6466c374d3c973c44d67c3916a3d
jbhampton1 <jbhampton1@crimson.ua.edu>
2024-09-11 14:34:26 -0500

    CaveMADE

---
+++ Assets/Scripts/MinigameTrigger.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
+public class MinigameTrigger : MonoBehaviour {
+    public Vector3 startPos;
+    public Vector3 spd;
+    public GameObject obj;
+    public bool touched;
+    public float timeDelay;
+    public int GameName;
+    public TMP_Text instructionsText;
+    public string GameInstructions;
+    // Start is called before the first frame update
+    void Start() {
+        instructionsText.text = GameInstructions;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        StartCoroutine(parallaxFunc());
+    }
+
+    IEnumerator parallaxFunc() {
+        yield return new WaitForSeconds(timeDelay);
+        //Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        if (!touched) {
+            obj.transform.position += spd;
+        } else {
+            obj.transform.position = startPos;
+            touched = false;
+        }
+
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Wagon") {
+            instructionsText.gameObject.SetActive(true);
+        } else if (other.tag == "ParallaxTrigger") {
+            touched = true;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.tag == "Wagon") {
+            //instructionsText.gameObject.SetActive(true);
+            if (Input.GetKey(KeyCode.X)) {
+                SceneManager.LoadScene(GameName);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Wagon") {
+            instructionsText.gameObject.SetActive(false);
+        }
+
+    }
+}
---
+++ Assets/Scripts/parallax.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour
+{
+    public Vector3 startPos;
+    public Vector3 endPos;
+    public Vector3 spd;
+    public GameObject obj;
+    public bool touched;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("The time is now: " + Time.time);
+        if (!touched){
+            obj.transform.position += spd;
+        }else{
+            obj.transform.position = startPos;
+            touched = false;
+        }
+
+    }
+    private void OnTriggerEnter(Collider other){
+        if(other.tag == "ParallaxTrigger")
+            touched = true;
+    }
+}
